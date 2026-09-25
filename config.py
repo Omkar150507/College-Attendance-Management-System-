5ed8ccd9ba4c5056f091efa4e63a1c4616201e39
Omkar150507 <shiv157303@gmail.com>
2026-09-25 12:14:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# College Attendance System - main branding settings
+COLLEGE_NAME = "Y.N.P. College of Pharmacy"
+COLLEGE_TAGLINE = "Attendance Management System"
+LOGO_FILE = "images/college_logo.png"
